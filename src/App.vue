29657d70bf6ae6f95477e17e215038d443e9c16e
unknown <zrow.8390@gmail.com>
2024-12-14 19:12:05 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1></h1>
+    <button @click="goToExternalSite">  کلیک کنید </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    goToExternalSite() {
+      let urlParams = new URLSearchParams(window.location.search);
+
+      const code =(urlParams.get('code'));
+      window.open("https://gnaf2.post.ir/pdf/postalcodes/plate/" + code, '_blank');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+button {
+  color: crimson;
+  padding: 10px 20px;
+  font-size: 16px;
+}
+</style>
